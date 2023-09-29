4a0c8fbe4ea1830f5cf6b23f1ec3b237b951c1b3
PapiLuee <41023873+LueDev@users.noreply.github.com>
2023-09-29 12:19:36 -0400

    Passed all tests

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(arr, name){
+    const result = arr.filter((driver) => driver.toLowerCase() === name.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(arr, name){
+    //slicing iterates through a string and is quicker to use than using for of/for loop 
+    const result = arr.filter((driver) => name.slice() === driver.slice(0, name.length))
+    return result
+}
+
+function matchName(arr, driverName){
+    const result = arr.filter((driverObj) =>{
+
+        //destructure the obj attributes to compare individually
+        let {name, hometown} = driverObj
+        if (name === driverName){return true}
+    })
+
+    return result
+}
+
